f10604e8a267ce8a0c784969d8787c4d9fdf915d
Damir Mansurov <rave9373@gmail.com>
2021-04-28 21:15:20 +0300

    fixed query2

--- HotelBookingBot/Commands/BookingCommand.cs
+++ HotelBookingBot/Commands/BookingCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
+using HotelBookingBot.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Telegram.Bot;
@@ -54,11 +55,24 @@ namespace HotelBookingBot.Commands
 			});
 			var booking = botClient.Bookings[update.CallbackQuery.From.Id];
 			await using var context = new DataContext(botClient.Configuration.GetConnectionString("Db"));
-			var hotel = context.Hotels.Include(i => i.City).Include(i => i.HotelRooms)
-				.FirstOrDefault(x => x.City.Name == booking.CityName && x.Stars == booking.HotelStarsType && 
-				                     x.HotelRooms.Exists(hotelRoom => 
-					                     hotelRoom.Price >= booking.PriceFilter.Item1 && 
-					                     hotelRoom.Price <= booking.PriceFilter.Item2 && hotelRoom.RoomType == booking.RoomType));
+			var hotelRooms = context.HotelRooms.Where(hotelRoom =>
+				hotelRoom.Price >= booking.PriceFilter.Item1 &&
+				hotelRoom.Price <= booking.PriceFilter.Item2 && hotelRoom.RoomType == booking.RoomType).Select(x=>x.HotelId).ToList();
+			
+			var hotels = context.Hotels.Include(i => i.City)
+				.Where(x => x.City.Name == booking.CityName && x.Stars == booking.HotelStarsType).ToList();
+
+			Hotel hotel = null;
+
+			foreach (var item in hotels)
+			{
+				if (hotelRooms.Contains(item.Id))
+				{
+					hotel = item;
+					break;
+				}
+			}
+			
 			if (hotel == null)
 			{
 				await _telegramBotClient.SendTextMessageAsync(update.CallbackQuery.From.Id, "Отель по вашим параметрам не найден! Попробовать снова?", replyMarkup: keyboardFail);
